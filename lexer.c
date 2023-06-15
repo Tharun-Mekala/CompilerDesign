06e0a951de7667c7fc1c6cb7d3351282ca4cc9b8
Tharun-Mekala <107202427+Tharun-Mekala@users.noreply.github.com>
2023-06-15 19:37:09 +0530

    lexer.c

--- lexer.c
+++ lexer.c
@@ -4,19 +4,23 @@
 
 #define MAX_TOKEN_LEN 100
 
-enum TokenType {
+enum TokenType
+{
     NUMBER, ID, KEYWORD, SYMBOL, OPERATOR, ERROR,
 };
 
-const char *TokenTypeStrings[] = {
+const char *TokenTypeStrings[] = 
+{
     "NUMBER","ID","KEYWORD","SYMBOL","OPERATOR","ERROR",
 };
 
-struct Token {
+struct Token 
+{
     enum TokenType type;
     char value[MAX_TOKEN_LEN];
 };
-int isKeyword(const char *str) {
+int isKeyword(const char *str) 
+{
     static const char *keywords[] = {
         "auto","break","case","char","const","continue","default","do",
         "double","else","enum","extern","float","for","goto","if",
@@ -33,56 +37,60 @@ int isKeyword(const char *str) {
     return 0;
 }
 
-struct Token getNextToken(const char *input, int *start) {
+struct Token getNextToken(const char *input, int *start) 
+{
     int len = strlen(input);
     int lineIndex = 0;
     char c = input[*start];
     while (*start < len)
-	 {
+    {
         if (isspace(c)) 
-		{
+	{
             (*start)++;
             c = input[*start];
             continue;
         }
         if (isdigit(c))
-		{
+	{
             struct Token t = {NUMBER, ""};
-            do {
+            do 
+	    {
                 t.value[lineIndex++] = c;
                 (*start)++;
                 c = input[*start];
             } while (isdigit(c));
             return t;
-        }
-        if (isalpha(c)) 
-		{
+         }
+         if (isalpha(c)) 
+	 {
             struct Token t = {ID, ""};
-            do {
+            do 
+	    {
                 t.value[lineIndex++] = c;
                 (*start)++;
                 c = input[*start];
             } while (isalnum(c));
             t.type = isKeyword(t.value) ? KEYWORD : ID;
             return t;
-        }
-        if (ispunct(c)) 
-		{
-            struct Token t = {SYMBOL, ""};
-            t.value[lineIndex++] = c;
+          }
+	  if (ispunct(c)) 
+	  {
+             struct Token t = {SYMBOL, ""};
+             t.value[lineIndex++] = c;
+             (*start)++;
+             t.type = strchr("+-*/%><=", c) != NULL? OPERATOR : SYMBOL;
+             return t;
+           }
+           struct Token t = {ERROR, ""};
+           t.value[lineIndex++] = c;
             (*start)++;
-            t.type = strchr("+-*/%><=", c) != NULL? OPERATOR : SYMBOL;
-            return t;
-        }
-        struct Token t = {ERROR, ""};
-        t.value[lineIndex++] = c;
-        (*start)++;
-        return t;
+           return t;
     }
     struct Token t = {EOF, ""};
     return t;
 }
-int main() {
+int main()
+{
     char input[1000];
     printf("Enter a string: ");
     scanf("%[^\n]s", input);
